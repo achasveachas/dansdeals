231b3cca4f52fe15362478b78a0a9a1ecc28f9a1
Yechiel Kalmenson <chilikkal@gmail.com>
2016-06-24 03:29:44 +0000

    built method to scrape deals

--- lib/dansdeals/deals.rb
+++ lib/dansdeals/deals.rb
@@ -1,5 +1,5 @@
 class DansDeals::Deals
-  attr_accessor :headline, :url, :dead
+  attr_accessor :headline, :url
   def self.latest
     self.scrape_deals
   end
@@ -7,8 +7,16 @@ class DansDeals::Deals
   def self.scrape_deals
     deals = []
 
-    doc = Nokogiri::HTML(open("www.dansdeals.com"))
+    doc = Nokogiri::HTML(open("http://www.dansdeals.com"))
 
+    doc.css(".post").each do |post|
+      deal = {
+        :post_title => post.css(".posttitle").text,
+        :post_body => post.css(".entry").inner_text
+      }
+      deals << deal
+    end
+    binding.pry
     deals
   end
 end
