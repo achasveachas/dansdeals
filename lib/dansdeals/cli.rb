829270cd32aa1075df9d3429e37582f206c5383e
Yechiel Kalmenson <chilikkal@gmail.com>
2016-06-24 01:55:46 +0000

    fixed some edge cases

--- lib/dansdeals/cli.rb
+++ lib/dansdeals/cli.rb
@@ -17,10 +17,12 @@ class DansDeals::CLI
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the deal you want more info on or type list to see the deals again or type exit:"
+      puts "Enter the number of the deal you want more info on. You can also type \"list\" to see the deals again or type \"exit\" to leave:"
       input = gets.strip.downcase
-      if input.to_i > 0
-        puts @deal[input.to_i - 1].headline
+      if input.to_i.between?(1, @deal.length)
+        puts @deal[input.to_i - 1]
+      elsif input.to_i > @deal.length
+        puts "Sorry, there is no such deal."
       elsif input == "list"
         list_deals
       elsif input == "exit"
