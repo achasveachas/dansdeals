43268d70268d37874f06cf33ce7dd7bbc4255657
Yechiel Kalmenson <chilikkal@gmail.com>
2016-06-24 02:19:45 +0000

    added scraping dependencies

--- lib/dansdeals.rb
+++ lib/dansdeals.rb
@@ -1,3 +1,7 @@
+require "open-uri"
+require "nokogiri"
+require "pry"
+
 require_relative "./dansdeals/version"
 require_relative "./dansdeals/cli"
 require_relative "./dansdeals/deals"
--- lib/dansdeals/deals.rb
+++ lib/dansdeals/deals.rb
@@ -1,18 +1,14 @@
 class DansDeals::Deals
   attr_accessor :headline, :url, :dead
   def self.latest
+    self.scrape_deals
+  end
 
+  def self.scrape_deals
+    deals = []
 
-    deal_1 = self.new
-    deal_1.headline = "Raffle To Win $25,000 Has Been Launched!"
-    deal_1.url = "http://www.dansdeals.com/archives/90042"
-    deal_1.dead = false
+    doc = Nokogiri::HTML(open("www.dansdeals.com"))
 
-    deal_2 = self.new
-    deal_2.headline = "50% Off Plus $14.99 Off Any Shutterfly Hardcover Photo Book!"
-    deal_2.url = "http://www.dansdeals.com/archives/90064"
-    deal_2.dead = true
-
-    [deal_1, deal_2]
+    deals
   end
 end
